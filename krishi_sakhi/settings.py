563b8f8b4181a6748a2d599d091dad47d7e028ae
ManojKumarC0112 <manojkchandrashekar@gmail.com>
2026-03-06 07:40:32 +0530

    chore: add Render deployment scripts and configuration

--- krishi_sakhi/settings.py
+++ krishi_sakhi/settings.py
@@ -77,12 +77,19 @@ WSGI_APPLICATION = 'krishi_sakhi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# If Render provides a Postgres URL, use it
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=False)
+if db_from_env:
+    DATABASES['default'].update(db_from_env)
 
 
 # Password validation
